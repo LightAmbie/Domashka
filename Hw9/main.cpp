d2ef5f6bf1b50937df9148a4d79b7441cf4e0af8
LightAmbie <lightambie@gmail.com>
2022-04-24 10:13:33 +0300

    ___

--- Hw9/main.cpp
+++ Hw9/main.cpp
@@ -10,16 +10,16 @@ int main()
 	{
 		if (i<4)
 		{
-			p[i-1] = new Apartment(1000 * i);
+			p[i-1] = new Apartment(1000 * i * 2);
 		}
 		else if (i<7)
 		{
-			p[i - 1] = new Car(200 * i);
+			p[i - 1] = new Car(200 * i * 2);
 		}
 
 		else
 		{
-			p[i - 1] = new CountryHouse(500 * i);
+			p[i - 1] = new CountryHouse(500 * i * 2);
 		}
 	}
 	for (int i = 0; i < 7; i++)
